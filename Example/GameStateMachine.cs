2db12087140fd5b2ab1ebe18734c3ae0456f0e3a
Eva Gt-Gd <eva.gerbert.gaillard@gmail.com>
2018-12-18 21:40:26 +0100

    Add source code and example

---
+++ Example/GameStateMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    MainMenu,
+    GameMenu,
+    InGame,
+    Restarting,
+    Exiting, 
+}
+
+public enum GameCommand
+{
+    Start,
+    Back
+}
+
+public class GameStateMachine : StateMachine<GameState, GameCommand>
+{
+    public GameStateMachine() : base(GameState.InGame)
+    {
+        transitions.Add(new StateTransition<GameState, GameCommand>     (GameState.MainMenu,    GameCommand.Start), GameState.InGame);
+        transitions.Add(new StateTransition<GameState, GameCommand>     (GameState.MainMenu,    GameCommand.Back),  GameState.Exiting);
+        transitions.Add(new StateTransition<GameState, GameCommand>     (GameState.InGame,      GameCommand.Start), GameState.GameMenu);
+        transitions.Add(new StateTransition<GameState, GameCommand>     (GameState.InGame,      GameCommand.Back),  GameState.Restarting);
+        transitions.Add(new StateTransition<GameState, GameCommand>     (GameState.GameMenu,    GameCommand.Start), GameState.InGame);
+        transitions.Add(new StateTransition<GameState, GameCommand>     (GameState.GameMenu,    GameCommand.Back),  GameState.MainMenu);
+    }
+}
